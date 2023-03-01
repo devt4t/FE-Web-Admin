3c8c2c6cf6f9f19975a633d404c41a05e2182875
iyasmzn <58409100+iyasmzn@users.noreply.github.com>
2023-03-01 16:39:11 +0700

    show trees in monitoring 2 and bump to v1.0.6

---
+++ src/views/Activity/components/monitoring2/dialogs/TitleInfo.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-dialog 
+        v-model="showDialog" 
+        max-width="500px"
+        content-class="rounded-xl"
+    >
+        <v-card>
+            <v-card-title class="d-flex align-center">
+                <p class="mb-0 blue--text">
+                    <v-icon color="blue">mdi-information</v-icon>
+                    Information
+                </p>
+                <v-divider class="mx-2"></v-divider>
+                <v-icon color="red" @click="() => showDialog = false">mdi-close-circle</v-icon>
+            </v-card-title>
+            <v-card-text>
+                <h3 class="mb-3 mt-5 green--text"><v-icon color="green">mdi-forest</v-icon> Show total trees amount.</h3>
+                <p>Data ini hanya mengambil data dari <b>"Realisasi Tanam / Monitoring 1"</b>. <br>Jumlah pohon yang tertera adalah jumlah pohon yang <b>ditanam</b> dan <b>hidup</b>, dan yang sudah <b>diverifikasi</b> oleh Unit Manager.</p>
+            </v-card-text>
+            <v-card-actions>
+                <v-divider class="mr-2"></v-divider>
+                <v-btn rounded outlined color="blue" class="pl-1" @click="() => showDialog = false">
+                    <v-icon class="mr-1">mdi-check-circle</v-icon>
+                    Okay
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        showDialog: {
+            get: function () {
+                return this.show
+            },
+            set: function (val) {
+                if (val == false) this.$emit('close', 'titleInfo')
+            }
+        }
+    }
+
+}
+</script>
