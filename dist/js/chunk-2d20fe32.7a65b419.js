0385965524e299c7e0fcdb76482c1277b61952ba
Eaunggelia Triandi <teaunggelia@gmail.com>
2023-07-21 17:05:57 +0700

    update desa last build

---
+++ dist/js/chunk-2d20fe32.7a65b419.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20fe32"],{b619:function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},o=[],u={props:{show:{type:Boolean,default:!1}}},s=u,a=t("2877"),c=Object(a["a"])(s,l,o,!1,null,null,null);n["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d20fe32.7a65b419.js.map
