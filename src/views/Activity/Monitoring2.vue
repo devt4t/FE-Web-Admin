9654aa8139ee1fc0ac976ce89d40216b242a13d8
EaunggeliaTriandi <88504314+EaunggeliaTriandi@users.noreply.github.com>
2024-03-19 14:25:53 +0700

    update delete mo condition

--- src/views/Activity/Monitoring2.vue
+++ src/views/Activity/Monitoring2.vue
@@ -542,7 +542,7 @@
               </v-btn>
             </v-list-item>
             <!-- v-if="item.total_tree_monitoring != item.total_hidup" -->
-            <v-list-item>
+            <v-list-item v-if="item.total_tree_monitoring != item.total_hidup">
               <v-btn
                   dark
                   rounded
