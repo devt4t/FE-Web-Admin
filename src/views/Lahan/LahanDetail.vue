f11db1efd95a1901e968ad173bc3f84d16e4bd96
edosetia <edosetiawan.me@gmail.com>
2024-09-25 13:59:49 +0700

    new

--- src/views/Lahan/LahanDetail.vue
+++ src/views/Lahan/LahanDetail.vue
@@ -202,6 +202,8 @@
                     mouData &&
                     mouData.mou_status == 1
                   "
+                  v-for="(lahan, i) in mouData.lahans"
+                  :key="`lahan-${i}`"
                 >
                   <v-icon>mdi-printer-outline</v-icon>
                   <span>Print Appendix</span>
@@ -1669,15 +1671,15 @@ export default {
 
       // get data mou
       try {
-        const {data: resMou} = await this.$_api.get('farmer-mou/find', {
+        const { data: resMou } = await this.$_api.get("farmer-mou/find", {
           farmer_no: result.main_lahan.farmer_no,
-          program_year: this.$_config.programYear.model
-        })
-        console.log('resMou', resMou)
-        this.mouData = resMou
+          program_year: this.$_config.programYear.model,
+        });
+        console.log("resMou", resMou);
+        this.mouData = resMou;
       } catch (err) {
-        console.log('getData() farmer-mou/find error',err)
-        this.mouData = null
+        console.log("getData() farmer-mou/find error", err);
+        this.mouData = null;
       }
 
       this.openMaps();
