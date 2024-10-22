ecb634b083d2512266f27f484a54f48d15eda175
Rizqi T4T <risqi.maulana@trees4trees.org>
2024-10-21 23:19:10 -0700

    vilage v2

--- src/views/Utilities/vilage/vilageConfig.js
+++ src/views/Utilities/vilage/vilageConfig.js
@@ -159,20 +159,102 @@ export default [
     },
   },
   {
-    id: "kode_ta",
-    label: "Kode TA",
+    id: "mu_no_from_ta_pivot",
+    label: "Managemen Unit",
     methods: {
-      list: { transform: "no-empty" },
-      detail: { transform: "no-empty" },
+      list: {
+        view_data: "mu_name",
+        transform: "no-empty"
+      },
+      detail: {
+        view_data: "mu_name",
+        transform: "no-empty"
+      },
       create: {
         type: "text",
         col_size: 6,
         validation: ["required"],
       },
       update: {
+        type: "select",
+        col_size: 6,
+        validation: ["required"],
+        setter: "mu_no",
+        getter: "new-utilities/management-units",
+        param: {
+          active: '1',
+          limit: '10',
+          offset: '0'
+        },
+        option: {
+          getter: "id",
+          default_label: "mu_name",
+          list_pointer: {
+            code: "mu_no",
+            label: "name",
+            display: ["name"],
+          },
+        },
+      },
+      filter: {
+        validation: ["required"],
+        type: "select",
+        col_size: 6,
+        main: true,
+        getter: "new-utilities/management-units",
+        setter: "mu_no",
+        param: {
+          page: 1,
+          per_page: 10,
+        },
+        option: {
+          getterKey: "data",
+          list_pointer: {
+            code: "mu_no",
+            label: "name",
+            display: ["name"],
+          },
+        },
+      },
+    },
+  },
+  {
+    id: "ta_from_pivot",
+    label: "Target Area",
+    methods: {
+      list: {
+        view_data: "ta_from_pivot_name",
+        transform: "no-empty"
+      },
+      detail: {
+        view_data: "ta_from_pivot_name",
+        transform: "no-empty"
+      },
+      create: {
         type: "text",
         col_size: 6,
         validation: ["required"],
+      },
+      update: {
+        type: "select",
+        col_size: 6,
+        validation: ["required"],
+        setter: "area_code",
+        getter: "new-utilities/target-areas",
+        param: {
+          active: '1',
+          limit: '10',
+          offset: '0'
+        },
+        option: {
+          getter: "id",
+          default_label: "ta_from_pivot_name",
+          list_pointer: {
+            code: "area_code",
+            label: "name",
+            display: ["name"],
+          },
+        },
       },
       filter: {
         validation: ["required"],
@@ -215,10 +297,10 @@ export default [
     },
   },
   {
-    id: "program_year",
+    id: "program_year_from_pivot",
     label: "Tahun Program",
     methods: {
-      list: { transform: "no-empty" },
+      list: true,
       detail: { transform: "no-empty" },
       create: {
         type: "text",
@@ -239,14 +321,48 @@ export default [
       list: { transform: "active", },
       detail: { transform: "active" },
       create: {
-        type: "text",
+        type: "select",
         col_size: 6,
         validation: ["required"],
+        option: {
+          default_options: [
+            {
+              name: "Tidak Aktif",
+              code: "0",
+            },
+            {
+              name: "Aktif",
+              code: "1",
+            }
+          ],
+          list_pointer: {
+            code: "code",
+            label: "name",
+            display: ["name"],
+          },
+        },
       },
       update: {
-        type: "text",
+        type: "select",
         col_size: 6,
         validation: ["required"],
+        option: {
+          default_options: [
+            {
+              name: "Tidak Aktif",
+              code: "0",
+            },
+            {
+              name: "Aktif",
+              code: "1",
+            }
+          ],
+          list_pointer: {
+            code: "code",
+            label: "name",
+            display: ["name"],
+          },
+        },
       },
       filter: false,
     },
@@ -258,7 +374,7 @@ export default [
       list: {
         view_data: "scooping_visits_potential_status",
         class: {
-          // 1: "badge bg-warning",
+          1: "badge bg-warning",
           2: "badge bg-info",
           3: "badge bg-info",
           4: "badge bg-info",
@@ -269,7 +385,7 @@ export default [
       detail: {
         view_data: "scooping_visits_potential_status",
         class: {
-          // 1: "badge bg-warning",
+          1: "badge bg-warning",
           2: "badge bg-info",
           3: "badge bg-info",
           4: "badge bg-info",
@@ -278,14 +394,72 @@ export default [
         transform: "potential-vilage",
       },
       create: {
-        type: "text",
+        type: "select",
         col_size: 6,
         validation: ["required"],
+        option: {
+          default_options: [
+            {
+              name: "Tidak Potensial",
+              code: "0",
+            },
+            {
+              name: "Belum Pengecekan",
+              code: "1",
+            },
+            {
+              name: "Potensial Carbon",
+              code: "2",
+            },
+            {
+              name: "Potensial Non-Carbon",
+              code: "3",
+            },
+            {
+              name: "Potensial Carbon & Non-Carbon",
+              code: "4",
+            },
+          ],
+          list_pointer: {
+            code: "code",
+            label: "name",
+            display: ["name"],
+          },
+        },
       },
       update: {
-        type: "text",
+        type: "select",
         col_size: 6,
         validation: ["required"],
+        option: {
+          default_options: [
+            {
+              name: "Tidak Potensial",
+              code: "0",
+            },
+            {
+              name: "Belum Pengecekan",
+              code: "1",
+            },
+            {
+              name: "Potensial Carbon",
+              code: "2",
+            },
+            {
+              name: "Potensial Non-Carbon",
+              code: "3",
+            },
+            {
+              name: "Potensial Carbon & Non-Carbon",
+              code: "4",
+            },
+          ],
+          list_pointer: {
+            code: "code",
+            label: "name",
+            display: ["name"],
+          },
+        },
       },
       filter: {
         validation: ["required"],
