e4a9b3b010634747352e1f8869944153c2ac5c69
Eaunggelia Triandi <teaunggelia@gmail.com>
2024-11-07 01:04:12 +0700

    update planting hole list and validation

--- src/views/ActivityNew/planting_hole/PlantingHole.vue
+++ src/views/ActivityNew/planting_hole/PlantingHole.vue
@@ -1,6 +1,17 @@
 <template>
-    <!-- <geko-base-crud :config="config" :refreshKey="refreshKey" :hideUpdate="true" :hideDelete="true" :hideCreate="true"/> -->
-    <div class="under-development">
+    <geko-base-crud :config="config" :refreshKey="refreshKey" :hideUpdate="true" :hideDelete="true" :hideCreate="true">
+        <template v-slot:list-bottom-action="{ item }">
+            <v-btn v-if="item.is_validate" variant="danger" small class="mt-2" @click="onUnverif(item)">
+                <v-icon left small>mdi-undo</v-icon>
+                <span>Unverifikasi</span>
+            </v-btn>
+            <v-btn v-else variant="success" small class="mt-2" @click="onVerif(item)">
+                <v-icon left small>mdi-check-bold</v-icon>
+                <span>Verifikasi</span>
+            </v-btn>
+        </template>
+    </geko-base-crud>
+    <!-- <div class="under-development">
         <div class="wrapper">
         <div class="text-wrapper">
             <h3>{{ $route.name.replace(/([a-z])([A-Z])/g, "$1 $2") }} Module</h3>
@@ -13,29 +24,29 @@
             style="height: 70vh"
         />
         </div>
-    </div>
+    </div> -->
 </template>
 
 <script>
+import plantingHoleDetail from "./PlantingHoleDetail.vue"
 import maintenanceAnimation from "@/assets/lottie/maintenance.json";
 import plantingHoleConfig from "./PlantingHoleConfig";
 import LottieAnimation from "lottie-web-vue";
 export default {
     components: {
         LottieAnimation,
+        plantingHoleDetail
     },
     name: "crud-planting-hole",
     watch: {},
     data() {
         return {
+            refreshKey: 1,
             config: {
                 title: "Lubang Tanam",
-                model_api: null,
                 getter: "new-planting-hole/list/main",
-                totalDataKey: 'count_record',
-                // setter: "ams/post_create",
-                // update: "ams/post_update",
-                // delete: "ams/post_delete",
+                getterDataKey: "data",
+                totalDataKey: 'total',
                 
                 deleteKey: "id",
                 pk_field: null,
@@ -44,19 +55,56 @@ export default {
                     setter: "program_year",
                     },
                 },
+                filter_api: {
+                    typegetdata: 'all'
+                },
                 permission: {
                     create: "lubang-tanam-create",
                     read: "lubang-tanam-list",
-                    // update: "lubang-tanam-update",
                     detail: "lubang-tanam-detail",
-                    // lookup: "lubang-tanam-lookup",
-                    // delete: "lubang-tanam-delete",
                 },
                 slave: [],
                 fields: plantingHoleConfig,
             },
             lottie: maintenanceAnimation,
         };
+    },
+    mounted() {
+        const user = JSON.parse(localStorage.getItem("User"));
+        this.user = user;
+    },
+    methods:{
+        async onVerif(item) {
+            const prompt = await this.$_alert.confirm('Verifikasi Penilikan Lubang?', 'Apakah Anda Yakin Untuk Melakukan VERIFIKASI Data Penilikan Lubang?', 'Ya, Verifikasi', 'Batal', true)
+            if (prompt.isConfirmed) {
+                this.$_api.post('new-planting-hole/update/validation', {
+                    ph_form_no: item.ph_form_no,
+                    validation_type: 'validate',
+                    validate_by: this.user.employee_no
+                })
+                .then(() => {
+                    this.$_alert.success('Data Penilikan Lubang Berhasil Diverifikasi')
+                    this.refreshKey += 1
+                })
+            }
+            console.log(item.ph_form_no)
+        },
+        async onUnverif(item) {
+            const prompt = await this.$_alert.confirm('Unverifikasi Penilikan Lubang?', 'Apakah Anda Yakin Untuk Melakukan UNVERIFIKASI Data Penilikan Lubang?', 'Ya, Unverifikasi', 'Batal', true)
+            if (prompt.isConfirmed) {
+                this.$_api.post('new-planting-hole/update/validation', {
+                    ph_form_no: item.ph_form_no,
+                    validation_type: 'unvalidate',
+                    validate_by: this.user.employee_no,
+                })
+                .then(() => {
+                    this.$_alert.success('Pelatihan berhasil diunverifikasi')
+                    this.refreshKey += 1
+                })
+
+            }
+            console.log(item.ph_form_no)
+        },
     },
 };
 </script>
