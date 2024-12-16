67501ed835dc87397ca5ce17b98a1a263a366352
edosetia <edosetiawan.me@gmail.com>
2024-12-16 14:45:00 +0700

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 export default {
-  version: "2.4.0",
-  baseUrl: "http://localhost:8000/api/",
-  baseUrlUpload: "https://t4tadmin.kolaborasikproject.com",
+  version: "2.5.0",
+  baseUrl: "https://api.t4t-api.org/api/",
+  baseUrlUpload: "https://geko-asset.t4t-api.org",
   baseUrlExport: "https://geko-export.t4t-api.org/",
-  baseUrlNursery: "https://backend.t4t-api.org/api",
+  baseUrlNursery: "https://api-nursery.t4t-api.org/api",
   mapBoxApi:
     "pk.eyJ1IjoiZGV2dDR0IiwiYSI6ImNtMm1zc3M0NDBxeXcyanFzd25ndnlva3cifQ.pBINatrH39ajJ_ILIHoGsg",
   mapBoxStyle: "mapbox://styles/mapbox/satellite-streets-v12",
