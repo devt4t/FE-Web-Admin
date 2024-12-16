61040609c85f31c7a007272f4eda200c0ca25cd5
edosetia <edosetiawan.me@gmail.com>
2024-12-16 16:56:26 +0700

    update api

--- src/utils/config.prod.js
+++ src/utils/config.prod.js
@@ -1,7 +1,7 @@
 export default {
   version: "2.2.0",
-  baseUrl: "https://t4tapi.kolaborasikproject.com/api/",
-  baseUrlUpload: "https://t4tadmin.kolaborasikproject.com",
+  baseUrl: "https://geko.t4t-api.org/api/",
+  baseUrlUpload: "https://geko-asset.t4t-api.org",
   baseUrlExport: "https://geko-export.t4t-api.org/",
   baseUrlNursery: "https://api-nursery.t4t-api.org/api",
   mapBoxApi:
