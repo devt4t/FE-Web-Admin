8bf30049e8e02e6b3d4c971dec6d19ae264e3625
Eaunggelia Triandi <teaunggelia@gmail.com>
2024-12-23 12:22:42 +0700

    update monitoring 2

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 export default {
   version: "2.5.0",
-  baseUrl: "https://api.t4t-api.org/api/",
-  // baseUrl: "https://t4tapi.kolaborasikproject.com/api/",
-  baseUrlUpload: "https://geko-asset.t4t-api.org",
-  // baseUrlUpload: "https://t4tadmin.kolaborasikproject.com",
+  // baseUrl: "https://api.t4t-api.org/api/",
+  baseUrl: "https://t4tapi.kolaborasikproject.com/api/",
+  // baseUrlUpload: "https://geko-asset.t4t-api.org",
+  baseUrlUpload: "https://t4tadmin.kolaborasikproject.com",
   baseUrlExport: "https://geko-export.t4t-api.org/",
   baseUrlNursery: "https://api-nursery.t4t-api.org/api",
   mapBoxApi:
