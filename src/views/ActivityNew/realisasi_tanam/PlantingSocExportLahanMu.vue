6d8b7843efb236ec93edad5a6979c71e12b77601
Rizqi T4T <risqi.maulana@trees4trees.org>
2024-12-29 23:53:36 +0700

    export penilikan lubang

--- src/views/ActivityNew/realisasi_tanam/PlantingSocExportLahanMu.vue
+++ src/views/ActivityNew/realisasi_tanam/PlantingSocExportLahanMu.vue
@@ -389,20 +389,20 @@ export default {
                     continue;
                 }
 
-                const trees = await this.$_api
-                    .get("GetTreesAll")
-                    .then((res) => {
-                        return res.data.result.data;
-                    })
-                    .catch((err) => {
-                        console.log("err", err);
-                        return false;
-                    });
+                // const trees = await this.$_api
+                //     .get("GetTreesAll")
+                //     .then((res) => {
+                //         return res.data.result.data;
+                //     })
+                //     .catch((err) => {
+                //         console.log("err", err);
+                //         return false;
+                //     });
 
-                if (!trees) {
-                    this.loading = false;
-                    continue;
-                }
+                // if (!trees) {
+                //     this.loading = false;
+                //     continue;
+                // }
 
                 const configUrl = {
                     pdf: `${this.$_config.baseUrlExport}export/farmer-land-polygon/pdf`,
@@ -545,7 +545,8 @@ export default {
                     responseType: "arraybuffer",
                     data: {
                         data: this.exportData,
-                        trees
+                        exportBy: this.exportBy,
+                        trees: trees
                     },
                     headers: {
                         "content-type": "application/json",
@@ -599,6 +600,7 @@ export default {
                     : "";
 
                 let offset = 0;
+                let trees = [];
                 while (true) {
                     const result = await this.getExportDataCarbon(_mu, offset);
                     if (!result) {
@@ -622,6 +624,7 @@ export default {
                     } else {
                         console.log(result, offset)
                         this.exportData = [...this.exportData, ...result.data]
+                        trees = result.trees;
                         if (result.data.length < 100) break;
                         offset += 100;
                     }
@@ -666,7 +669,8 @@ export default {
                     responseType: "arraybuffer",
                     data: {
                         data: this.exportData,
-                        // trees: trees
+                        exportBy: this.exportBy,
+                        trees: trees
                     },
                     headers: {
                         "content-type": "application/json",
@@ -720,6 +724,7 @@ export default {
                     : "";
 
                 let offset = 0;
+                let trees = [];
                 while (true) {
                     const result = await this.getExportDataCarbon(_ff, offset);
                     if (!result) {
@@ -743,6 +748,7 @@ export default {
                     } else {
                         console.log(result, offset)
                         this.exportData = [...this.exportData, result.data]
+                        trees = result.trees;
                         if (result.data.length < 100) break;
                         offset += 100;
                     }
@@ -787,7 +793,8 @@ export default {
                     responseType: "arraybuffer",
                     data: {
                         data: this.exportData,
-                        // trees: trees
+                        exportBy: this.exportBy,
+                        trees: trees
                     },
                     headers: {
                         "content-type": "application/json",
