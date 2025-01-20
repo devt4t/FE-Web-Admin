2f9c1487db667e03ae1e8857f6ec35d08bfa443f
edosetia <edosetiawan.me@gmail.com>
2025-01-20 15:11:41 +0700

    add status pupuk distribution

--- src/views/Distribution/UnloadDistribution/UnloadAllocationDetail.vue
+++ src/views/Distribution/UnloadDistribution/UnloadAllocationDetail.vue
@@ -158,10 +158,33 @@
                       >
                         <v-icon class="mr-1">mdi-clock-alert </v-icon> Proses Distribusi Belum Selesai!
                       </v-btn>
+                      <!-- <v-btn
+                        v-if="true"
+                        variant="warning" small class="mt-2"
+                      >
+                        <v-icon class="mr-1">mdi-clock-alert </v-icon> Proses Distribusi Belum Selesai!
+                      </v-btn> -->
                     </template>
                     <template v-slot:item.total_pupuk="{ item }">
                       <p v-if="item.is_pupuk_load == 0" >0</p>
                       <p v-else>{{item.total_pupuk}}</p>
+                    </template>
+                    <template v-slot:item.status_pupuk="{ item }">
+                      <div class="d-flex flex-row align-items-center">
+                        <span class="badge"
+                          :class="{
+                            'bg-light': item.is_pupuk_distributed == 0,
+                            'bg-success': item.is_pupuk_distributed == 1,
+                            'bg-warning': item.is_pupuk_distributed == 2,
+                            'bg-danger': item.is_pupuk_distributed == 3,
+                          }"
+                        >
+                          <span v-if="item.is_pupuk_distributed == 0">Belum Diterima</span>
+                          <span v-if="item.is_pupuk_distributed == 1">Pupuk Lengkap</span>
+                          <span v-if="item.is_pupuk_distributed == 2">Pupuk Kurang</span>
+                          <span v-if="item.is_pupuk_distributed == 3">Pupuk Kelebihan</span>
+                        </span>
+                      </div>
                     </template>
                     <template v-slot:item.acceptance_progress="{ item }">
                       <v-progress-circular
