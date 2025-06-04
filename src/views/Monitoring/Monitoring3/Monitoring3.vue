bbf416e9466e565f454d93adc6db6274617a1bea
Rizqi T4T <risqi.maulana@trees4trees.org>
2025-06-04 14:15:25 +0700

    fix: update unverification button condition to check if item is populated

--- src/views/Monitoring/Monitoring3/Monitoring3.vue
+++ src/views/Monitoring/Monitoring3/Monitoring3.vue
@@ -41,7 +41,7 @@
                 <span>Hapus</span>
             </v-btn>
             <v-btn variant="danger" small class="d-flex flex-row align-items-center mt-2"
-                @click="onUnverif(item)" v-if="item.is_verified != 0">
+                @click="onUnverif(item)" v-if="item.is_verified != 0 && item.is_populated == 0">
                 <v-icon small>mdi-backspace</v-icon>
                 <span>Unverifikasi</span>
             </v-btn>
