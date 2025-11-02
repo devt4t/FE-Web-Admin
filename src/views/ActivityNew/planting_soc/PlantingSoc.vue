45763f01c414c7240367e1987c669d144895761a
Rizqi <maulanadevelop@gmail.com>
2025-11-03 00:48:41 +0700

    update

--- src/views/ActivityNew/planting_soc/PlantingSoc.vue
+++ src/views/ActivityNew/planting_soc/PlantingSoc.vue
@@ -331,6 +331,7 @@ export default {
                     
                     // console.log(totalSeconds, item.verified);
                     if (totalSeconds <= 0) {
+                        this.refreshKey += 1
                         // this.resetDistributionDate(item.soc_no,item.updated_at);
                         clearInterval(this.intervals[item.soc_no]);
                     }
