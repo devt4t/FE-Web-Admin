5d1372b06ff57bf5e8b28dffb400e3189ab3a47e
kokatmx <umyuhm611@gmail.com>
2026-05-25 10:10:13 +0700

    feat(village): add program_year 2026 on adjust form assign village and add to config also

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
     "pk.eyJ1IjoiZGV2dDR0IiwiYSI6ImNtMm1zc3M0NDBxeXcyanFzd25ndnlva3cifQ.pBINatrH39ajJ_ILIHoGsg",
   mapBoxStyle: "mapbox://styles/mapbox/satellite-streets-v12",
   programYear: {
-    model: "2024",
-    options: ["2020", "2021", "2022", "2023", "2024", "2025"],
+    model: "2025",
+    options: ["2020", "2021", "2022", "2023", "2024", "2025", "2026"],
   },
 };
