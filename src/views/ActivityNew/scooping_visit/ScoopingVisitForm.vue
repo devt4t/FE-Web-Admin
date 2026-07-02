52d2509f328825adfb9004f9babf0c6140dd7418
kokatmx <umyuhm611@gmail.com>
2026-07-02 10:25:23 +0700

    fix(scooping-visit): typo condition disable

--- src/views/ActivityNew/scooping_visit/ScoopingVisitForm.vue
+++ src/views/ActivityNew/scooping_visit/ScoopingVisitForm.vue
@@ -365,7 +365,7 @@
               type: 'number',
               setter: 'total_dusun',
               view_data: 'total_dusun',
-            }" :disabled="(val, data) => data && data.is_updated == 1" />
+            }" :disabled="formData.is_updated == 1" />
           </v-col>
 
           <v-col md="6">
