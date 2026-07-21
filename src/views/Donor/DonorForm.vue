cfbf8c99a39578398737882e35febec8d2bc02ad
kokatmx <umyuhm611@gmail.com>
2026-07-21 12:06:11 +0700

    fix(donor): update program year selection

--- src/views/Donor/DonorForm.vue
+++ src/views/Donor/DonorForm.vue
@@ -270,28 +270,19 @@
                 code: 'code',
                 display: ['label'],
               },
-              default_options: [
-                {
-                  label: '2020',
-                  code: 2020,
-                },
-                {
-                  label: '2021',
-                  code: 2021,
-                },
-                {
-                  label: '2022',
-                  code: 2022,
-                },
-                {
-                  label: '2023',
-                  code: 2023,
-                },
-                {
-                  label: '2024',
-                  code: 2024,
-                },
-              ],
+              default_options: $_config.programYear.options.map(
+                (x) => {
+                  return {
+                    label: x,
+                    code: x,
+                  };
+                }
+              ),
+              list_pointer: {
+                code: 'code',
+                label: 'label',
+                display: ['label'],
+              },
             },
           }" />
         </v-col>
